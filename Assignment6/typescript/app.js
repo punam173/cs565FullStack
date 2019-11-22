392b52e3b1d14d2a799daf0e2680185edb7a1256
punam173 <punampal173@gmail.com>
2019-11-22 11:19:00 -0800

    Delete app.js

--- Assignment6/typescript/app.js
+++
@@ -1,7 +0,0 @@
-var numbers = [12, 23, 62, 34, 19, 40, 4, 9];
-console.log(numbers.filter(function (n) {
-    return n > 30;
-}));
-//  Or use shorthand function notation. 
-//  (Also called arrow function) 
-console.log(numbers.filter(function (n) { return n > 30; }));
